480996dbcfa2d459b4d5adef7a2fea504e6da6f0
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-04 10:52:04 +0700

    register DI configuration (#6)

---
+++ BookShop/BookShop.Infrastructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using BookShop.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookShop.Infrastructure.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    connectionString: configuration["ConnectionStrings:DefaultConnection"],
+                    sqlServerOptionsAction: sqlOptions =>
+                    {
+                        sqlOptions.EnableRetryOnFailure(
+                            maxRetryCount: 5,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null);
+                    }),
+            contextLifetime: ServiceLifetime.Scoped,
+            optionsLifetime: ServiceLifetime.Singleton);
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
