75c1314934e7abc0b787a156185debb47845d2c2
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-05 11:20:44 +0700

    define generic repository interface (#9)

---
+++ BookShop/BookShop.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace BookShop.Domain.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IEnumerable<T>> ListAsync();
+    Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate);
+    Task<IEnumerable<T>> ListAsync<TFilter>(TFilter filter)
+        where TFilter : class;
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(Guid id);
+    Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+    Task<bool> ExistsAsync(Guid id);
+}
