6e1a51a3ebd2d85c0da92a1bbe47ca75c03ea3f6
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-05 22:16:52 +0700

    implement user repo extending generic repo (#9)

---
+++ BookShop/BookShop.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookShop.Domain.Entities;
+
+namespace BookShop.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByIdWithProfileAsync(Guid id);
+    Task<bool> EmailExistsAsync(string email);
+}
