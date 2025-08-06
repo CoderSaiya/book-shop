321a117960dbda855390bc6c81724efe6dd4f8c2
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-06 15:11:21 +0700

    implement unit of work pattern (#9)

---
+++ BookShop/BookShop.Infrastructure/Persistence/Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using BookShop.Domain.Interfaces;
+
+namespace BookShop.Infrastructure.Persistence.Data.Repositories;
+
+public class UnitOfWork(
+    AppDbContext context, 
+    IAuthorRepository authors, 
+    IBookRepository books, 
+    IUserRepository users, 
+    IRefreshTokenRepository refreshes, 
+    IPublisherRepository publishers
+    ) : IUnitOfWork
+{
+    public IAuthorRepository Authors { get; } = authors;
+    public IBookRepository Books { get; } = books;
+    public IUserRepository Users { get; } = users;
+    public IRefreshTokenRepository Refreshes { get; } = refreshes;
+    public IPublisherRepository Publishers { get; } = publishers;
+
+    public async Task<int> SaveAsync()
+        => await context.SaveChangesAsync();
+}
