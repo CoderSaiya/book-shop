b69b9538f57dccc40aaf75bc6624e519a2ec8fbe
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-06 15:10:33 +0700

    implement refresh token repo extending generic repo (#9)

---
+++ BookShop/BookShop.Domain/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,8 @@
+﻿using BookShop.Domain.Entities;
+
+namespace BookShop.Domain.Interfaces;
+
+public interface IRefreshTokenRepository
+{
+    Task<RefreshToken?> GetByTokenAsync(string token);
+}
---
+++ BookShop/BookShop.Infrastructure/Persistence/Data/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookShop.Domain.Entities;
+using BookShop.Domain.Interfaces;
+
+namespace BookShop.Infrastructure.Persistence.Data.Repositories;
+
+public class RefreshTokenRepository(AppDbContext context) : GenericRepository<RefreshToken>(context)
+{
+    
+}
