6c0c65f3b8db57f88577a52cb384746b1053168d
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-07 22:27:14 +0700

    establish dto for book endpoint (#12)

---
+++ BookShop/BookShop.Application/DTOs/Req/CreateBookReq.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BookShop.Application.DTOs.Req;
+
+public record CreateBookReq(
+    Guid AuthorId,
+    Guid PublisherId,
+    string Title,
+    List<IFormFile> Images,
+    decimal? Price = 0m,
+    int? Stock = 0,
+    string? Description = null,
+    DateTime PublishingDate = default
+    );
---
+++ BookShop/BookShop.Application/DTOs/Res/BookRes.cs
@@ -0,0 +1,13 @@
+﻿namespace BookShop.Application.DTOs.Res;
+
+public record BookRes(
+    Guid BookId,
+    string AuthorName,
+    string PublisherName,
+    string Title,
+    string Description,
+    int Stock,
+    List<string> Images,
+    string PublishedDate,
+    bool IsSold
+    );
