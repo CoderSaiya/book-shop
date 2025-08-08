6093549672cc2faad0db5b6d53c183ae2744b91a
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-08 15:37:03 +0700

    introduce MailSettings for SMTP host, credentials and sender info

---
+++ BookShop/BookShop.Domain/Specifications/MailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Domain.Specifications;
+
+public class MailSettings
+{
+    public string Host { get; set; } = null!;
+    public int Port { get; set; }
+    public string User { get; set; } = null!;
+    public string Pass { get; set; } = null!;
+    public string SenderName { get; set; } = null!;
+    public string SenderEmail { get; set; } = null!;
+}
