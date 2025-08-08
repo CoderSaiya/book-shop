a6d1f09e73a0431b95a5bfdb299da97b9ca5c2dd
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-08 21:52:03 +0700

    bind rabbit mq setting and mail setting via Options pattern (#12)

--- BookShop/BookShop.API/Program.cs
+++ BookShop/BookShop.API/Program.cs
@@ -1,3 +1,4 @@
+using BookShop.Domain.Specifications;
 using BookShop.Infrastructure.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.Configure<RabbitMqSettings>(builder.Configuration.GetSection("RabbitMqSettings"));
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
