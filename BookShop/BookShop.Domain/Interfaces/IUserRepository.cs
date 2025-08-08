5a9c42fe9c88921a7bfba19088a85c11dbfd77c9
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-08 15:49:50 +0700

    implement SearchAsync with keyword‐based, paginated EF Core query (#9)

--- BookShop/BookShop.Domain/Interfaces/IUserRepository.cs
+++ BookShop/BookShop.Domain/Interfaces/IUserRepository.cs
@@ -4,6 +4,7 @@ namespace BookShop.Domain.Interfaces;
 
 public interface IUserRepository : IRepository<User>
 {
+    Task<IEnumerable<User>> SearchAsync(string keyword, int page = 1, int pageSize = 50);
     Task<User?> GetByEmailAsync(string email);
     Task<User?> GetByIdWithProfileAsync(Guid id);
     Task<bool> EmailExistsAsync(string email);
