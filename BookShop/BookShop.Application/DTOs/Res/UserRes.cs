1a6a758cb47e3868c4bec066a3e817363b03c438
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-08 17:59:02 +0700

    establish dto for user endpoint (#12)

---
+++ BookShop/BookShop.Application/DTOs/Res/UserRes.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Application.DTOs.Res;
+
+public record UserRes(
+    Guid UserId,
+    string Email,
+    string Name,
+    string Phone,
+    string Address,
+    string? Avatar,
+    DateTime CreatedAt
+    );
