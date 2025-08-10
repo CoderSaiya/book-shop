5d0c4702b55de1dcf01e67d7591560f38924d2b6
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-10 23:16:04 +0700

    implement basic author controller (#14)

---
+++ BookShop/BookShop.API/Controllers/AuthorController.cs
@@ -0,0 +1,53 @@
+﻿using BookShop.Application.DTOs.Req;
+using BookShop.Application.DTOs.Res;
+using BookShop.Application.Interface;
+using BookShop.Domain.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthorController(IAuthorService authorService) : Controller
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(GlobalResponse<IEnumerable<UserRes>>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAlls()
+    {
+        var authors = await authorService.GetAll();
+        return Ok(GlobalResponse<IEnumerable<AuthorRes>>.Success(authors));
+    }
+    
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(GlobalResponse<AuthorRes>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
+    {
+        var author = await authorService.GetById(id);
+        return Ok(GlobalResponse<AuthorRes>.Success(author));
+    }
+    
+    [HttpPut("profile/{id:guid}")]
+    [Consumes("multipart/form-data")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> UpdateProfile([FromRoute] Guid id, [FromForm] UpdateAuthorReq req)
+    {
+        await authorService.Update(id, req);
+        return NoContent();
+    }
+    
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        await authorService.Delete(id);
+        return NoContent();
+    }
+}
