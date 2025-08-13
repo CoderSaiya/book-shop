6b52a94a478c1ba0785c86e5c8dd12ca3f165331
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-13 09:39:33 +0700

    add sale field in book

---
+++ BookShop/BookShop.Infrastructure/Persistence/Migrations/20250812114658_UpdateBook_SaleField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookShop.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBook_SaleField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Sale",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sale",
+                table: "Books");
+        }
+    }
+}
